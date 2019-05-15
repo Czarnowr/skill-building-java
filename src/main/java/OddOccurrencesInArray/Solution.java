4a16249118e81ba6aa8803658f1ef2f10fff0a2e
Radek <czarnowr@gmail.com>
2019-05-15 13:57:37 +0200

    comments and minor refactoring

--- src/main/java/OddOccurrencesInArray/Solution.java
+++ src/main/java/OddOccurrencesInArray/Solution.java
@@ -5,22 +5,26 @@ import java.util.List;
 
 class Solution {
     int solution(int[] A) {
-
+        //linked list to hold numbers
         List<Integer> linkedListOfNumbers = new LinkedList<>();
 
+        // transferring numbers to a linked list
         for (int number : A) {
             linkedListOfNumbers.add(number);
         }
 
+        // take a number, find the first and last index the number is on
         while(linkedListOfNumbers.size() > 0) {
             int number = linkedListOfNumbers.get(0);
             int firstIndex = linkedListOfNumbers.indexOf(number);
             int lastIndex = linkedListOfNumbers.lastIndexOf(number);
 
+            // if the indexes are the same = odd number
             if (firstIndex == lastIndex) {
                 return linkedListOfNumbers.get(firstIndex);
             }
 
+            //remove numbers on those indexes
             linkedListOfNumbers.remove(lastIndex);
             linkedListOfNumbers.remove(firstIndex);
         }
--- src/main/java/OddOccurrencesInArray/SolutionOnArray.java
+++ src/main/java/OddOccurrencesInArray/SolutionOnArray.java
@@ -5,22 +5,25 @@ import java.util.Arrays;
 public class SolutionOnArray {
     int solution(int[] A) {
 
+        // if Array consists of one number - return number
         if (A.length == 1) {
             return A[0];
         }
 
+        // sort the array
         Arrays.sort(A);
 
-        for (int i = 0; i < A.length - 1; i++) {
+        // go through the Array two numbers at a time and compare them
+        for (int i = 0; i < A.length - 1; i = i + 2) {
+            // compare a pair of numbers, if the pair does not match, return the first number
             if (A[i] != A[i + 1]) {
                 return A[i];
+            // if all pairs have been checked and only one number remains, return it
             } else if (i == A.length - 3) {
                 return A[A.length - 1];
-            } else {
-                i = i + 1;
             }
         }
-        return 0;
+        return -1;
     }
 }
 
