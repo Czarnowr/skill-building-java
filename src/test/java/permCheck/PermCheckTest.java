3e6dd1b821e9053c6c1808b94eb702b503839d36
Radek <czarnowr@gmail.com>
2019-05-16 13:26:36 +0200

    Solution and more tests

--- src/test/java/permCheck/PermCheckTest.java
+++ src/test/java/permCheck/PermCheckTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsEqual.equalTo;
 
-class PermCheckTest {
+public class PermCheckTest {
     private static PermCheck solver = new PermCheck();
 
     @Test
@@ -25,6 +25,84 @@ class PermCheckTest {
     @Test
     public void PermCheckTest_checkIfArray4_1_3IsAPermutation_Result0() {
         int[] arrayOfNumbers = {4, 1, 3};
+
+        int expectedResult = 0;
+
+        //Act
+        int isPermutation = solver.solution(arrayOfNumbers);
+
+        //Assert
+        Assert.assertThat(isPermutation, is(equalTo(expectedResult)));
+    }
+
+    @Test
+    public void PermCheckTest_checkIfArray4IsAPermutation_Result0() {
+        int[] arrayOfNumbers = {4};
+
+        int expectedResult = 0;
+
+        //Act
+        int isPermutation = solver.solution(arrayOfNumbers);
+
+        //Assert
+        Assert.assertThat(isPermutation, is(equalTo(expectedResult)));
+    }
+
+    @Test
+    public void PermCheckTest_checkIfArray1IsAPermutation_Result1() {
+        int[] arrayOfNumbers = {1};
+
+        int expectedResult = 1;
+
+        //Act
+        int isPermutation = solver.solution(arrayOfNumbers);
+
+        //Assert
+        Assert.assertThat(isPermutation, is(equalTo(expectedResult)));
+    }
+
+    @Test
+    public void PermCheckTest_checkIfArray4_3IsAPermutation_Result0() {
+        int[] arrayOfNumbers = {4, 3};
+
+        int expectedResult = 0;
+
+        //Act
+        int isPermutation = solver.solution(arrayOfNumbers);
+
+        //Assert
+        Assert.assertThat(isPermutation, is(equalTo(expectedResult)));
+    }
+
+    @Test
+    public void PermCheckTest_checkIfArray1_2IsAPermutation_Result1() {
+        int[] arrayOfNumbers = {1, 2};
+
+        int expectedResult = 1;
+
+        //Act
+        int isPermutation = solver.solution(arrayOfNumbers);
+
+        //Assert
+        Assert.assertThat(isPermutation, is(equalTo(expectedResult)));
+    }
+
+    @Test
+    public void PermCheckTest_checkIfArray4_6IsAPermutation_Result0() {
+        int[] arrayOfNumbers = {4, 6};
+
+        int expectedResult = 0;
+
+        //Act
+        int isPermutation = solver.solution(arrayOfNumbers);
+
+        //Assert
+        Assert.assertThat(isPermutation, is(equalTo(expectedResult)));
+    }
+
+    @Test
+    public void PermCheckTest_checkIfArray1_3IsAPermutation_Result0() {
+        int[] arrayOfNumbers = {1, 3};
 
         int expectedResult = 0;
 
