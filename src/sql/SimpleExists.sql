2e2e9f9f777085c10617469f659f0805e280e105
Radek <czarnowr@gmail.com>
2019-07-10 10:55:46 +0200

    refactoring for clarity

--- src/sql/SimpleExists.sql
+++ src/sql/SimpleExists.sql
@@ -22,9 +22,9 @@
 
 SELECT id, name
 FROM departments
-WHERE EXISTS (
-    SELECT name
-    FROM sales
-    WHERE sales.department_id = departments.id
-      AND price > 98
-    );
+WHERE EXISTS(
+              SELECT name
+              FROM sales
+              WHERE sales.department_id = departments.id
+                AND price > 98
+          );
