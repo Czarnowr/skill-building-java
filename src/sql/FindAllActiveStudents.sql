703343e5fe5ccfbca02b2ae785b650b0efe546cd
Radek <czarnowr@gmail.com>
2019-07-11 07:06:40 +0200

    Find All Active Students solution

---
+++ src/sql/FindAllActiveStudents.sql
@@ -0,0 +1,27 @@
+-- noinspection SqlResolveForFile
+--
+-- Create a simple SELECT query to display student information of all ACTIVE students.
+--
+-- students TABLE STRUCTURE:
+--   Id
+--   FirstName
+--   LastName
+--   IsActive
+--
+-- Note: IsActive (true or false)
+
+-- Version 1 (SQLite):
+SELECT Id,
+       FirstName,
+       LastName,
+       IsActive
+FROM students
+WHERE IsActive = 1;
+
+-- Version 2 (PostgradeSQL):
+SELECT Id,
+       FirstName,
+       LastName,
+       IsActive
+FROM students
+WHERE IsActive = true;
