38e905fe2988dc220544d4a4d814f878de3f0de4
Radek <czarnowr@gmail.com>
2019-07-29 13:48:00 +0200

    added additional test for 0 start and end point

--- src/test/java/codility/lesson5_prefixSums_CountDiv/CountDiv1Test.java
+++ src/test/java/codility/lesson5_prefixSums_CountDiv/CountDiv1Test.java
@@ -7,7 +7,7 @@ public class CountDiv1Test {
     private static CountDiv1 solver = new CountDiv1();
 
     @Test
-    public void CountDiv1_numbers6To10DivisibleBy2_3() {
+    public void CountDiv1_numbers6To11DivisibleBy2_3() {
         //Arrange
         int A = 6;
         int B = 11;
@@ -18,5 +18,33 @@ public class CountDiv1Test {
 
         //Assert
         Assertions.assertThat(actual).isEqualTo(3);
+    }
+
+    @Test
+    public void CountDiv1_numbers5To10DivisibleBy2_3() {
+        //Arrange
+        int A = 5;
+        int B = 10;
+        int K = 2;
+
+        //Act
+        int actual = solver.solution(A, B, K);
+
+        //Assert
+        Assertions.assertThat(actual).isEqualTo(3);
+    }
+
+    @Test
+    public void CountDiv1_numbers0To0DivisibleBy2_1() {
+        //Arrange
+        int A = 0;
+        int B = 0;
+        int K = 2;
+
+        //Act
+        int actual = solver.solution(A, B, K);
+
+        //Assert
+        Assertions.assertThat(actual).isEqualTo(1);
     }
 }
