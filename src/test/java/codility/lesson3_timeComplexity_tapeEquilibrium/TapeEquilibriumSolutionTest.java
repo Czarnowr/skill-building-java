cad17058893dc2c3b9a0585cb98edc33225c946a
Radek <czarnowr@gmail.com>
2019-07-29 10:05:51 +0200

    refactor package structure to flatten project structure

---
+++ src/test/java/codility/lesson3_timeComplexity_tapeEquilibrium/TapeEquilibriumSolutionTest.java
@@ -0,0 +1,30 @@
+package codility.lesson3_timeComplexity_tapeEquilibrium;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+public class TapeEquilibriumSolutionTest {
+    private static TapeEquilibriumSolution solver = new TapeEquilibriumSolution();
+
+    @Test
+    public void TapeEquilibrium_getMinimalDifferenceForArray3_1_2_4_3_Result1() {
+        int[] arrayOfNumbers = {3, 1, 2, 4, 3};
+
+        //P = 1, difference = |3 − 10| = 7
+        //P = 2, difference = |4 − 9| = 5
+        //P = 3, difference = |6 − 7| = 1
+        //P = 4, difference = |10 − 3| = 7
+
+        int expectedMinimalDifference = 1;
+
+        //Act
+        int minimalDifference = solver.solution(arrayOfNumbers);
+
+        //Assert
+        Assert.assertThat(minimalDifference, is(equalTo(expectedMinimalDifference)));
+    }
+
+}
