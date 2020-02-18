c754eca13511b2f061303938e88d8dcab538733f
Radoslaw Czarnowski <radoslaw.czarnowski@epam.com>
2020-02-18 19:15:56 +0100

    Add StringReverser implementation using two character arrays

---
+++ src/test/java/basics/string_reverser/TwoCharacterArraysStringReverserTest.java
@@ -0,0 +1,9 @@
+package basics.string_reverser;
+
+public class TwoCharacterArraysStringReverserTest extends StringReverserTest {
+
+    @Override
+    protected StringReverser createInstance() {
+        return new TwoCharacterArraysStringReverser();
+    }
+}
